e6421ceee2c584219b39c9cfee0727d679e92f33
Sebastian <sebastian@squidex.io>
2021-01-19 21:24:56 +0100

    Added files.

---
+++ Squidex.Log/ConstantsLogAppender.cs
@@ -0,0 +1,28 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschränkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using System;
+
+namespace Squidex.Log
+{
+    public sealed class ConstantsLogAppender : ILogAppender
+    {
+        private readonly Action<IObjectWriter> objectWriter;
+
+        public ConstantsLogAppender(Action<IObjectWriter> objectWriter)
+        {
+            Guard.NotNull(objectWriter, nameof(objectWriter));
+
+            this.objectWriter = objectWriter;
+        }
+
+        public void Append(IObjectWriter writer, SemanticLogLevel logLevel, Exception? exception)
+        {
+            objectWriter(writer);
+        }
+    }
+}
